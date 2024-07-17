1969582007aa99d70ef9e94f7d19df0151e9a23e
Mariano Chun <marianochun01@gmail.com>
2024-07-17 19:55:49 -0300

    Add weather station - Observer pattern

---
+++ Intro/src/ObserverPattern/CurrentConditionDisplay.java
@@ -0,0 +1,23 @@
+package ObserverPattern;
+
+public class CurrentConditionDisplay implements Observer, DisplayElement{
+    private float temperature;
+    private float humidity;
+    private WeatherData weatherData;
+
+    public CurrentConditionDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Current conditions " + temperature + "C degrees and " + humidity + "% humidity");
+    }
+}
